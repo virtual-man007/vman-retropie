28814615d51c1bfe55ed3ae334da86bcaa75c667
virtual-man007 <centronic_solutions@hotmail.com>
2020-03-01 22:32:47 -0400

    removed space

--- apply_updates.sh
+++ apply_updates.sh
@@ -3,6 +3,5 @@
 
 cp -p /opt/retropie/configs/n64/retroarch.cfg /opt/retropie/configs/n64/retroarch.cfg.bkp
 cp ./opt/retropie/configs/n64/retroarch.cfg /opt/retropie/configs/n64/retroarch.cfg
-
 cp -p /opt/retropie/configs/all/runcommand.cfg /opt/retropie/configs/all/runcommand.cfg.bkp
 cp ./opt/retropie/configs/all/runcommand.cfg /opt/retropie/configs/all/runcommand.cfg
