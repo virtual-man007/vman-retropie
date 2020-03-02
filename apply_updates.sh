a37d628cf63b276f6e909be1c0840c32c1248dac
virtual-man007 <centronic_solutions@hotmail.com>
2020-03-01 22:40:31 -0400

    remove space

--- apply_updates.sh
+++ apply_updates.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # all the changes neccessary to apply the updates
-
 cp -p /opt/retropie/configs/n64/retroarch.cfg /opt/retropie/configs/n64/retroarch.cfg.bkp
 cp ./opt/retropie/configs/n64/retroarch.cfg /opt/retropie/configs/n64/retroarch.cfg
 cp -p /opt/retropie/configs/all/runcommand.cfg /opt/retropie/configs/all/runcommand.cfg.bkp
