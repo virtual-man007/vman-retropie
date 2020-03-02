5e93ddf89390840ba5b5ad6943ee65aae24e939c
virtual-man007 <centronic_solutions@hotmail.com>
2020-03-01 22:15:56 -0400

    added new apply updates.sh for local cp commands

---
+++ apply_updates.sh
@@ -0,0 +1,7 @@
+# all the changes neccessary to apply the updates
+
+cp -p /opt/retropie/configs/n64/retroarch.cfg /opt/retropie/configs/n64/retroarch.cfg.bkp
+cp ./opt/retropie/configs/n64/retroarch.cfg /opt/retropie/configs/n64/retroarch.cfg
+
+cp -p /opt/retropie/configs/all/runcommand.cfg /opt/retropie/configs/all/runcommand.cfg.bkp
+cp ./opt/retropie/configs/all/runcommand.cfg /opt/retropie/configs/all/runcommand.cfg
