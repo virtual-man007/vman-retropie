37ebc061f3737e984bd0c229ca510160b7212667
virtual-man007 <centronic_solutions@hotmail.com>
2020-03-02 09:34:21 -0400

    batch #1 all documented changes added and beta testing

--- apply_updates.sh
+++ apply_updates.sh
@@ -1,6 +1,34 @@
 #!/bin/sh
 # all the changes neccessary to apply the updates
-cp -p /opt/retropie/configs/n64/retroarch.cfg /opt/retropie/configs/n64/retroarch.cfg.bkp
+
+# change #1 n64 retroarch disable analogue fix control scheme for most games
+cp -p /opt/retropie/configs/n64/retroarch.cfg /opt/retropie/configs/n64/retroarch.cfg.VM
 cp ./opt/retropie/configs/n64/retroarch.cfg /opt/retropie/configs/n64/retroarch.cfg
-cp -p /opt/retropie/configs/all/runcommand.cfg /opt/retropie/configs/all/runcommand.cfg.bkp
+
+# change #2 runcommand optimization
+cp -p /opt/retropie/configs/all/runcommand.cfg /opt/retropie/configs/all/runcommand.cfg.VM
 cp ./opt/retropie/configs/all/runcommand.cfg /opt/retropie/configs/all/runcommand.cfg
+
+# change #3 emulators for specific games
+cp -p /opt/retropie/configs/all/emulators.cfg /opt/retropie/configs/all/emulators.cfg.VM
+cp ./opt/retropie/configs/all/emulators.cfg /opt/retropie/configs/all/emulators.cfg
+
+# change #4 all retroarch global file update
+cp -p /opt/retropie/configs/all/retroarch.cfg /opt/retropie/configs/all/retroarch.cfg.VM
+cp ./opt/retropie/configs/all/retroarch.cfg /opt/retropie/configs/all/retroarch.cfg
+
+# change #5 shaders update to use color mangler, avoid using bsnes_gamma, performance hit
+cp -p /opt/retropie/configs/all/retroarch/shaders/1arcade.glslp /opt/retropie/configs/all/retroarch/shaders/1arcade.glslp.VM
+cp ./opt/retropie/configs/all/retroarch/shaders/1arcade.glslp /opt/retropie/configs/all/retroarch/shaders/1arcade.glslp
+cp -p /opt/retropie/configs/all/retroarch/shaders/16bit.glslp /opt/retropie/configs/all/retroarch/shaders/16bit.glslp.VM
+cp ./opt/retropie/configs/all/retroarch/shaders/16bit.glslp /opt/retropie/configs/all/retroarch/shaders/16bit.glslp
+
+# change #6 shaders preset for hanheld
+cp ./opt/retropie/configs/all/retroarch/shaders/presets/Beetle VM/ /opt/retropie/configs/all/retroarch/shaders/presets/Beetle VM/
+cp ./opt/retropie/configs/all/retroarch/shaders/presets/mGBA/ /opt/retropie/configs/all/retroarch/shaders/presets/mGBA/
+
+# change #7 fix scummvm-residualvm to start properly
+cp ./home/pi/RetroPie/roms/scummvm/*.sh /home/pi/RetroPie/roms/scummvm/
+
+# change #8 Xeno Crisis use of right analogue stick configuration (8bitdo SN30 Pro)
+cp -R ./opt/retropie/configs/genesis/ /opt/retropie/configs/genesis/
