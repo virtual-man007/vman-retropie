5414678080e313f70772eda5c370d8ba226f42f4
virtual-man007 <centronic_solutions@hotmail.com>
2020-05-06 00:18:39 -0300

    # Roling back to 2020-04-18 fixplaybox update - by 2play! VMAN!

--- home/pi/RetroPie/extras+/.pb-fixes/retropiemenu/fixplaybox.sh
+++ home/pi/RetroPie/extras+/.pb-fixes/retropiemenu/fixplaybox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # All required fixes in case you break something 
 # Fix retropiemenu, es_systems.cfg etc.
-# PlayBox ToolKit v03.05.2020
+# PlayBox ToolKit v20.04.2020
 
 infobox=""
 infobox="${infobox}        __________.__                 __________                
@@ -36,53 +36,56 @@ function main_menu() {
         choice=$(dialog --backtitle "$BACKTITLE" --title " MAIN MENU " \
             --ok-label OK --cancel-label Exit \
             --menu "Which fix or action would you like to apply?" 25 75 20 \
-            1 "Fix:  Fix PlayBox Custom Retropie Menu	***Only from cli***" \
-            2 "Fix:  Set Your Region US/EU-JP/ALL (es_systems)" \
-            3 "Fix:  Repair BGM Emulator Mute Config File" \
-            4 "Fix:  Restore 2Play! Original Slideshow Images Set" \
-            5 "Fix:  Reset All RetroPie Controllers Configuration" \
-            6 "Fix:  Restore 2Play! Top CLi Commands History" \
-            7 "Fix:  Fix-Reset-Clean git for RetroPie Setup " \
-            8 "Fix:  Force Filesystem Recheck At Next Boot " \
-            9 "Tool:  Skyscraper By Lars Muldjord		***Only from cli***" \
-           10 "Tool:  OMXPlayer Volume Control Script" \
-           11 "Cleanup:  Clean all save, hi, dat etc files in roms folder" \
-           12 "Cleanup:  Cleanup Favorites & More		***Only from cli***" \
-           13 "Cleanup:  ES Auto Gamelists Cleanup		***Only from cli***" \
-           14 "Cleanup:  Filesystem Cache Cleanup" \
-           15 "Info:  Show Partitions & Space Info" \
-           16 "Info:  Show System Free Memory Info" \
-           17 "Info:  Show OS Version & Info" \
-           18 "Desktop:  Start Raspbian Desktop		***Only from cli***" \
-           19 "System:  System Update" \
-           20 "System:  System Full Info" \
-           21 "System:  System Shutdown" \
-           22 "System:  System Reboot" \
+            - "*** PLAYBOX TOOLKIT SELECTIONS ***" \
+			1 "Fix: Fix PlayBox Custom Retropie Menu	***Only from cli***" \
+            2 "Fix: Set Your Region US/EU-JP/ALL (es_systems)" \
+            3 "Fix: Repair JukeBox/PlayBox/Kodi/Power/Raspbian/Steam Systems" \
+            4 "Fix: Repair BGM Emulator Mute Config File" \
+            5 "Fix: Restore Music Set" \
+            6 "Fix: Restore Slideshow Images Set" \
+            7 "Fix: Reset All RetroPie Controllers Configuration" \
+            8 "Fix: Restore 2Play! Top CLi Commands History" \
+            9 "Fix: Fix-Reset-Clean git for RetroPie Setup " \
+           10 "Tool: Skyscraper By Lars Muldjord		***Only from cli***" \
+           11 "Tool: OMXPlayer Volume Control" \
+           12 "Cleanup: Clean all save, hi, dat etc files in roms folder" \
+           13 "Cleanup: Cleanup Favorites & More		***Only from cli***" \
+           14 "Cleanup: ES Auto Gamelists Cleanup		***Only from cli***" \
+           15 "Cleanup: Filesystem Cache Cleanup" \
+           16 "Info: Show Partitions & Space Info" \
+           17 "Info: Show System Free Memory Info" \
+           18 "Info: Show OS Version & Info" \
+           19 "Desktop: Start Raspbian Desktop		***Only from cli***" \
+           20 "System: System Update" \
+           21 "System: System Full Info" \
+           22 "System: System Shutdown" \
+           23 "System: System Reboot" \
             2>&1 > /dev/tty)
 
         case "$choice" in
             1) fix_rpiem  ;;
             2) fix_region  ;;
-            3) fix_bgm_py  ;;
-            4) fix_slideshow  ;;
-            5) fix_control  ;;
-            6) fix_cli_hist  ;;
-            7) git_rs  ;;
-            8) fschk_bt  ;;
-            9) skyscraper  ;;
-           10) omxvol  ;;
-           11) cl_saves  ;;
-           12) cl_xml  ;;
-           13) cl_es_gamelist  ;;
-           14) cl_cache  ;;
-           15) partitions  ;;
-           16) freemem  ;;
-           17) os_info  ;;
-           18) raspbian  ;;
-           19) os_update  ;;
-           20) sysinfo  ;;
-           21) shutdown_sc  ;;
-           22) reboot_sc  ;;
+            3) fix_roms  ;;
+            4) fix_bgm_py  ;;
+            5) fix_music  ;;
+            6) fix_slideshow  ;;
+            7) fix_control  ;;
+            8) fix_cli_hist  ;;
+            9) git_rs  ;;
+           10) skyscraper  ;;
+           11) omxvol  ;;
+           12) cl_saves  ;;
+           13) cl_xml  ;;
+           14) cl_es_gamelist  ;;
+           15) cl_cache  ;;
+           16) partitions  ;;
+           17) freemem  ;;
+           18) os_info  ;;
+           19) raspbian  ;;
+           20) os_update  ;;
+           21) sysinfo  ;;
+           22) shutdown  ;;
+           23) reboot  ;;
             *)  break ;;
         esac
     done
@@ -116,9 +119,21 @@ function fix_region() {
 	/home/pi/RetroPie/extras+/.pb-fixes/_scripts/region.sh	
 }
 
+function fix_roms() {
+	dialog --infobox "...Fixing..." 3 17 ; sleep 1
+	clear
+	rsync -av /home/pi/RetroPie/extras+/.pb-fixes/roms/jukebox /home/pi/RetroPie/roms/ && rsync -av /home/pi/RetroPie/extras+/.pb-fixes/roms/playbox /home/pi/RetroPie/roms/ && rsync -av /home/pi/RetroPie/extras+/.pb-fixes/roms/power /home/pi/RetroPie/roms/ && rsync -av /home/pi/RetroPie/extras+/.pb-fixes/roms/raspbian /home/pi/RetroPie/roms/ && rsync -av /home/pi/RetroPie/extras+/.pb-fixes/roms/steam /home/pi/RetroPie/roms/
+}
+
 function fix_bgm_py() {
 	dialog --infobox "...Fixing..." 3 17 ; sleep 1
 	cp /home/pi/RetroPie/extras+/.pb-fixes/bgm/.livewire.py /home/pi
+}
+
+function fix_music() {
+	dialog --infobox "...Fixing..." 3 17 ; sleep 1
+	clear
+	rsync -av /home/pi/RetroPie/extras+/.pb-fixes/music /home/pi/RetroPie/roms/
 }
 
 function fix_slideshow() {
@@ -144,16 +159,6 @@ function git_rs() {
 	dialog --infobox "...Fixing..." 3 17 ; sleep 1
 	cd RetroPie-Setup && git reset --hard && git clean -f -d
 }
-
-function fschk_bt() {
-	dialog --infobox "...Please Wait..." 3 22 ; sleep 1
-	clear
-	echo
-	echo "Please be patient... Screen will go black, pi's green activity led will be on while filsystem check. Once completed your system will reboot as normal."
-	sleep 5
-	sudo touch /forcefsck && sudo reboot
-}
-
 function skyscraper() {
 	dialog --infobox "...Starting..." 3 20 ; sleep 1
 	clear
@@ -239,20 +244,20 @@ function sysinfo() {
 	/home/pi/RetroPie/extras+/.pb-fixes/_scripts/2play_sysinfo.sh
 }
 
-function shutdown_sc() {
+function shutdown() {
 	dialog --infobox "...Shutting Down..." 3 23 ; sleep 1
 	clear
 	echo
-	echo "[OK System Will Shutdown now...]"
-#	sudo shutdown -h now
-	sudo shutdown -P now
+	echo "[OK] Shutting Down ... "
+	sudo shutdown -h now
+#	sudo killall emulationstation && sleep 3 && sudo shutdown -h now
 }
 
-function reboot_sc() {
+function reboot() {
 	dialog --infobox "...Starting..." 3 20 ; sleep 1
 	clear
 	echo
-	echo "[OK System Will Restart now...]"
+	echo "[OK] Rebooting ... "
 	sudo reboot
 }
 main_menu
